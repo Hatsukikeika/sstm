c308554f3fe6aa9992275c818a3f62a9bb62d3ab
unknown <xuegao60189@gmail.com>
2025-05-19 12:42:18 -0700

    update pom profile

--- cli/src/main/java/com/yrc/sstm/cli/Main.java
+++ cli/src/main/java/com/yrc/sstm/cli/Main.java
@@ -20,6 +20,7 @@ public class Main {
     public static void main(String[] args) {
         CommandLine cli = initArgs(args);
         if (cli == null) {
+            log.error("初始化失败...");
             return;
         }
 
